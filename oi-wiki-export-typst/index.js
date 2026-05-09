0eb57ee569ac3fbea181a04cf94613e6824a288c
Yuchen Fu <fuyuchen0904@163.com>
2026-05-09 22:07:42 +0800

    fix: remove colbreak from generated files

--- oi-wiki-export-typst/index.js
+++ oi-wiki-export-typst/index.js
@@ -155,7 +155,7 @@ async function main() {
         } else {
           labelHistory.push(dirName)
           if (depth === 0) {
-            result += '#colbreak()\n'
+            // chapter break handled by columns container
           }
           result += '{0} {1} <{2}>\n'.format('='.repeat(depth + 1), escape(key), dirName)
           if (depth === 0) {
